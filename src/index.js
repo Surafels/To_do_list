c34ec9e7a5a5b68fb76792d9c9b6acc8db04b26f
Surafels <ssurafel4@gmail.com>
2023-08-24 16:08:20 +0300

    Add function and import on index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-// import _ from 'lodash';
-// import './style.css';
-// import printMe from './print.js';
-// function component() {
-//     const element = document.createElement('div');
-//     const btn = document.createElement('button');
-//     // Lodash, currently included via a script, is required for this line to work
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     btn.innerHTML = 'Click me and check the console!';
-//     btn.onclick = printMe;
-  
-//     element.appendChild(btn);
-//     element.classList.add('hello');
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
+import { addList } from "./print.js"; 
+import './style.css';
+
+addList();
--- src/print.js
+++ src/print.js
@@ -16,3 +16,27 @@ const todoList = [
     index: 2,
   },
 ]
+function addList(){
+  const mylist = document.getElementById('listItem');
+  const listElements = todoList.map((todo) =>{
+    const div = document.createElement('div');
+    div.className ='listElement';
+    const description = document.createElement('p');
+    description.innerText =todo.description;
+    
+    const completed = document.createElement('div');
+    completed.innerText =todo.completed;
+    const clearBtn = document.createElement('button');
+    clearBtn.innerText = 'Clear';
+
+    div.appendChild(description);
+    div.appendChild(completed);
+    div.appendChild(clearBtn);
+
+    return div;
+
+  });
+  mylist.append(...listElements);
+}
+
+export {addList};
