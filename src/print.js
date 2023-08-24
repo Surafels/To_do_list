59f2533989799d3e707dccbbac3b5bb189d96d74
Surafels <ssurafel4@gmail.com>
2023-08-24 13:48:28 +0300

    Add todoList array of object

--- src/print.js
+++ src/print.js
@@ -1,3 +1,18 @@
-// export default function printMe() {
-//     console.log('I get called from print.js!');
-//   }
+
+const todoList = [
+  {
+    description: 'wash the dish',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'clean the dish',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'complete to do list project',
+    completed: true,
+    index: 2,
+  },
+]
