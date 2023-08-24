db2ad61a693f59dc9a4341fe3ff534fdbb7125dc
Surafels <ssurafel4@gmail.com>
2023-08-24 21:35:40 +0300

    Style the list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
-import { addList } from "./print.js"; 
 import './style.css';
+const list =document.getElementById('listItem');
+const addList = [
+  {
+    description: 'wash the dish',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'clean the dish',
+    completed: true,
+    index: 2,
+  },
+  {
+    description: 'complete to do list project',
+    completed: true,
+    index: 3,
+  },
+]
 
-addList();
+const itemList = function(){
+  list.innerHTML = '';
+  addList.forEach((item) =>{
+    list.innerHTML += `
+    <div class="ad-list">
+    <input type="checkbox" />
+    <p>${item.description}</p>
+    </div>
+
+    `
+  });
+};
+
+window.addEventListener('load',itemList );
