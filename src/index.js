febe37bb7489c1c1e55660499912dbf8d307dd74
Surafels <ssurafel4@gmail.com>
2023-08-25 10:22:10 +0300

    Add check box and styles container.

--- src/index.js
+++ src/index.js
@@ -21,23 +21,22 @@ function doListProject(){
   const list =document.getElementById('listItem');
 addList.forEach((todo) => {
   const div = document.createElement('div');
-  div.className = 'listElement';
+  div.className = 'taskContainer';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = todo.completed;
   const description = document.createElement('p');
   description.innerHTML = todo.description;
-  const completeTasks = document.createElement('div');
-  completeTasks.className = 'completeTasks';
-  const adBtn = document.createElement('button');
-  completeTasks.appendChild(adBtn);
-  adBtn.innerText ='Complete'
-  adBtn.className = 'adBtn';
+
   const remove = document.createElement('button');
   remove.className = 'removeBtn';
   remove.innerText ='Remove';
 
-  div.appendChild(completeTasks);
+ 
+  div.appendChild(checkbox);
   div.appendChild(description);
   div.appendChild(remove);
   list.appendChild(div);
 });
 }
-document.addEventListener('DOM ContentLoad', doListProject);  
+document.addEventListener('DOMContentLoaded', doListProject);  
