9928db1d56361a92d06fc4c16e8159b2ad2a87f5
Surafels <ssurafel4@gmail.com>
2023-08-25 23:32:01 +0300

    Add a function that remove added tasks

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const clearBtn = document.getElementsByClassName('clearBtn');
 const addIcon = document.getElementById('addIcon');
 const addInput = document.getElementById('addInput');
 const todoList=() =>{
-  let tasks = JSON.parse(localStorage.getItem(tasks)) || [];
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
   const storedTask =()=>{
-    localStorage.setItem(storedTask, JSON.stringify(tasks));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
   }
 
@@ -18,15 +18,39 @@ const todoList=() =>{
       index: tasks.length+1,
     }
     tasks.push(newTask);
-    storedTask()
-    console.log('new task add', newTask);
-    console.log("tasks", tasks);
+    storedTask();
+    location.reload();
   };
+  const removeTasks =(index) => {
+tasks= tasks.filter((task) => task.index !== index);
+storedTask();
+location.reload();
+  };
+  const listTasks =() =>{
+    listItem.innerHTML ='';
+  tasks.forEach((task) => {
+  const taskElement = document.createElement('li');
+  taskElement.textContent = task.description;
+  const ellipsisIcon = document.createElement('i');
+      ellipsisIcon.className = 'fas fa-ellipsis-v';
+      ellipsisIcon.addEventListener('click',() =>{
+        removeTasks(task.index);
+
+      })
+      taskElement.appendChild(ellipsisIcon);
+    listItem.appendChild(taskElement);
+
+    });
+    
+  }
+  listTasks();
   addIcon.addEventListener('click',(event) =>{
     event.preventDefault();
-    const description = addInput.value;
+    const description = addInput.value.trim();
     addTask(description);
   });
+
+ 
   
 }
 todoList();
