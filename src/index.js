dfa5b622085b2358259a9adeee555054cb4837b0
Surafels <ssurafel4@gmail.com>
2023-08-26 13:05:46 +0300

    Add local storage

--- src/index.js
+++ src/index.js
@@ -1,41 +1,7 @@
 import './style.css';
 
-const addList = [
-  {
-    description: 'wash the dish',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'clean the dish',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'complete to do list project',
-    completed: true,
-    index: 3,
-  },
-];
-
-function doListProject() {
-  const list = document.getElementById('listItem');
-  addList.forEach((todo) => {
-    const div = document.createElement('div');
-    div.className = 'taskContainer';
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = todo.completed;
-    const description = document.createElement('p');
-    description.innerHTML = todo.description;
-
-    const ellipsisIcon = document.createElement('i');
-    ellipsisIcon.className = 'fas fa-ellipsis-v';
-
-    div.appendChild(checkbox);
-    div.appendChild(description);
-    div.appendChild(ellipsisIcon);
-    list.appendChild(div);
-  });
+const myTodoList =() =>{
+const tasks =Json.parse(localStorage.getItem('tasks')) || [];
+const storedTasks =()=> { ('tasks', JSON.stringify(tasks));
 }
-document.addEventListener('DOMContentLoaded', doListProject);
+}
