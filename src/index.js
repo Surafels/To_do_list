be17cc5a4fad971b035bc40075f0aac0ffe0fdcd
Surafels <ssurafel4@gmail.com>
2023-08-26 13:14:53 +0300

    Add function for ellipsisIcon

--- src/index.js
+++ src/index.js
@@ -10,4 +10,14 @@ for (let i=0; i<myNodelist.length; i++){
   span.className = 'fas fa-ellipsis-v';
   myNodelist[i].appendChild(span);
 }
+const ellipsisIcon=document.querySelectorAll('.fas.fa-ellipsis-v');
+ellipsisIcon.forEach(function(icon) {
+icon.onclick =function() {
+  const listItem =this.parentElement;
+  listItem.style.display = 'none';
+  tasks.splice(i,1);
+  storedTasks();
+};
+});
+
 }
