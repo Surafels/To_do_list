9bedbb244d03ac819332f96c2ffec4acf65e09dd
Surafels <ssurafel4@gmail.com>
2023-08-26 13:10:24 +0300

    Add myNodelist

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@ const myTodoList =() =>{
 const tasks =Json.parse(localStorage.getItem('tasks')) || [];
 const storedTasks =()=> { ('tasks', JSON.stringify(tasks));
 }
+const myNodelist = document.getElementsByTagName('LI');
+for (let i=0; i<myNodelist.length; i++){
+  const span = document.createElement('i');
+  span.className = 'fas fa-ellipsis-v';
+  myNodelist[i].appendChild(span);
+}
 }
